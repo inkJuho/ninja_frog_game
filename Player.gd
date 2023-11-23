65303a1c2b8dfcc0fb2acc82188eb535e5cb0c5a
inkJuho <joshuat4014@gmail.com>
2023-11-23 11:57:41 -0800

    Original 1.0
    Original version

---
+++ Player.gd
@@ -0,0 +1,52 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 250
+const JUMP_FORCE = 500
+const GRAVITY = 25
+const MAX_FALL_SPEED = 500
+
+onready var anim_player = $AnimationPlayer
+onready var sprite = $Sprite
+
+var yvelo = 0
+var facing_right = false
+
+func _physics_process(delta):
+	var move_dir = 0
+	if Input.is_action_pressed("ui_right"):
+		move_dir += 1
+	if Input.is_action_pressed("ui_left"):
+		move_dir -= 1
+	move_and_slide(Vector2(move_dir * MOVE_SPEED, yvelo), Vector2(0,-1))
+	
+	var grounded = is_on_floor()
+	yvelo += GRAVITY
+	if grounded and Input.is_action_just_pressed("ui_up"):
+		yvelo = -JUMP_FORCE
+	if grounded and yvelo >= 5:
+		yvelo = 5
+	if yvelo > MAX_FALL_SPEED:
+		yvelo = MAX_FALL_SPEED
+		
+	if !facing_right and move_dir <0:
+		flip()
+	if facing_right and move_dir > 0:
+		flip()
+		
+	if grounded:
+		if move_dir == 0:
+			play_anim("idle")
+		else:
+			play_anim("Walk")
+	else:
+		play_anim("jump")
+	
+
+func flip():
+	facing_right = !facing_right
+	sprite.flip_h = !sprite.flip_h
+
+func play_anim(anim_name):
+	if anim_player.is_playing() and anim_player.current_animation == anim_name:
+		return
+	anim_player.play(anim_name)
